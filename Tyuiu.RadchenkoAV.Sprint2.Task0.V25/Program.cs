b10a05f263a2655c1c68f5c5e025bc715c2b70c5
alisa69 <aliska132007@gmail.ru>
2025-10-15 22:13:47 +0500

    Add project files.

---
+++ Tyuiu.RadchenkoAV.Sprint2.Task0.V25/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+using Tyuiu.RadchenkoAV.Sprint2.Task0.V25.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #2| Выполнил: Радченко А. В. | РППБ-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #25                                                              *");
+Console.WriteLine("* Выполнил: Радченко А. В.| РППБ-25-1                             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать консольную программу на C#, которая вычисляет значение         *");
+Console.WriteLine("* логических выражений                                                    *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ: x = 205 y = 159                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+int x = 205;
+int y = 159;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
