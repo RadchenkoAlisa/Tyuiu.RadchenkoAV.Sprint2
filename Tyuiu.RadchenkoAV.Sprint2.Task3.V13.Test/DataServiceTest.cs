c6d4b333654acba4618aa4bd63ce5f65dfd3c531
alisa69 <aliska132007@gmail.ru>
2025-10-16 14:59:31 +0500

    Task3

---
+++ Tyuiu.RadchenkoAV.Sprint2.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.RadchenkoAV.Sprint2.Task3.V13.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint2.Task3.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetLogicOperations()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(0.75, ds.Calculate(0));
+        }
+    }
+}
