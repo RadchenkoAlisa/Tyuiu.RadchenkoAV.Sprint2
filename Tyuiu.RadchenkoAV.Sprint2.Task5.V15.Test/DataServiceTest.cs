581b51df2ff35b56ad190ed7ebe3bdec1f0f4a39
alisa69 <aliska132007@gmail.ru>
2025-10-17 01:25:37 +0500

    Task5

---
+++ Tyuiu.RadchenkoAV.Sprint2.Task5.V15.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.RadchenkoAV.Sprint2.Task5.V15.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint2.Task5.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetLogicOperations()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("Понедельник", ds.FindDayName(1));
+            Assert.AreEqual("Вторник", ds.FindDayName(2));
+            Assert.AreEqual("Среда", ds.FindDayName(3));
+            Assert.AreEqual("Четверг", ds.FindDayName(4));
+            Assert.AreEqual("Пятница", ds.FindDayName(5));
+            Assert.AreEqual("Суббота", ds.FindDayName(6));
+            Assert.AreEqual("Воскресенье", ds.FindDayName(0));
+        }
+    }
+}
