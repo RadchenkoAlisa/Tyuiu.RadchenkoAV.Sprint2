8fb341cfa51980b6bb8b4585ea235e0ef81768ff
alisa69 <aliska132007@gmail.ru>
2025-10-17 23:57:03 +0500

    Task7

---
+++ Tyuiu.RadchenkoAV.Sprint2.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.RadchenkoAV.Sprint2.Task7.V15.Lib;
+namespace Tyuiu.RadchenkoAV.Sprint2.Task7.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetLogicOperations()
+        {
+            DataService ds = new DataService();
+            double x = 1.2;
+            double y = 1.2;
+            bool wait = true;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
